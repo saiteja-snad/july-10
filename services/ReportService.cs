4c42106c19d9cc604317effd41cb987a3e5026cd
saiteja-snad <saiteja.anumala@snaddevelopers.com>
2026-07-13 18:34:16 +0530

    Add files via upload

---
+++ services/ReportService.cs
@@ -0,0 +1,22 @@
+﻿using StudentManagementSystem.Services.Interfaces;
+
+public class ReportService : IReportService
+{
+    public async Task<object>
+        GetAttendanceSummaryAsync()
+    {
+        return await Task.FromResult(new { });
+    }
+
+    public async Task<object>
+        GetFeeDueReportAsync()
+    {
+        return await Task.FromResult(new { });
+    }
+
+    public async Task<object>
+        GetPerformanceReportAsync()
+    {
+        return await Task.FromResult(new { });
+    }
+}
