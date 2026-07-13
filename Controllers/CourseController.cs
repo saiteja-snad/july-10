b9263ca8364639620d78a56d4e43fb2b1092c083
saiteja-snad <saiteja.anumala@snaddevelopers.com>
2026-07-13 18:35:32 +0530

    Add files via upload

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,56 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SMS.DTOS.CourseDtos;
+using StudentManagementSystem.Services.Interfaces;
+using Swashbuckle.AspNetCore.Annotations;
+
+[ApiController]
+[Route("api/v1/courses")]
+[ApiVersion("1.0")]
+public class CourseController : ControllerBase
+{
+
+    private readonly ICourseService _service;
+
+    public CourseController(ICourseService service)
+    {
+        _service = service;
+    }
+    //=============================================
+    [HttpPost]
+    [Authorize(Roles = "ADMIN")]
+    [SwaggerOperation(Summary = "Create Course",Description = "Add a new course")]
+    public async Task<IActionResult> CreateCourse(CreateCourseRequestDTO dto)
+    {
+        return Ok(await _service.CreateCourseAsync(dto));
+    }
+    //==========================================================
+
+    [HttpGet]
+    [AllowAnonymous]
+    [SwaggerOperation(Summary = "Get Courses",Description = "Retrieve all courses")]
+    public async Task<IActionResult> GetCourses()
+    {
+        return Ok( await _service.GetAllCoursesAsync());
+    }
+    //=============================================================================
+
+    [HttpGet("{id}")]
+    [AllowAnonymous]
+    [SwaggerOperation(Summary = "Get Course By Id", Description = "Retrieve a course using course id")]
+    public async Task<IActionResult> GetCourse(int id)
+    {
+        return Ok(await _service.GetCourseByIdAsync(id));
+    }
+    //=======================================================================
+
+    [HttpPut("{id}")]
+    [Authorize(Roles = "ADMIN")]
+    [SwaggerOperation(Summary = "Update Course", Description = "Update course details")]
+    public async Task<IActionResult> UpdateCourse(int id,UpdateCourseRequestDTO dto)
+    {
+        return Ok( await _service.UpdateCourseAsync(id, dto));
+    }
+    //====================================================================
+}
---
+++ Controllers/ReportController.cs
@@ -0,0 +1,41 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using StudentManagementSystem.Services.Interfaces;
+using Swashbuckle.AspNetCore.Annotations;
+
+[ApiController]
+[Route("api/v1/reports")]
+[Authorize(Roles = "ADMIN")]
+[ApiVersion("1.0")]
+public class ReportController : ControllerBase
+{
+    private readonly IReportService _service;
+
+    public ReportController(IReportService service )
+    {
+        _service = service;
+    }
+    //=============================================================
+    [HttpGet("attendance-summary")]
+    [SwaggerOperation(Summary = "Attendance Report", Description = "Generate attendance summary report")]
+    public async Task<IActionResult>AttendanceSummary()
+    {
+        return Ok(await _service.GetAttendanceSummaryAsync());
+    }
+    //==========================================================================
+    [HttpGet("fee-dues")]
+    [SwaggerOperation(Summary = "Fee Due Report",Description = "Generate fee due report")]
+    public async Task<IActionResult>FeeDueReport()
+    {
+        return Ok(await _service.GetFeeDueReportAsync());
+    }
+    //===================================================================================
+
+    [HttpGet("performance-summary")]
+    [SwaggerOperation(Summary = "Performance Report",Description = "Generate academic performance report")]
+    public async Task<IActionResult>PerformanceSummary()
+    {
+        return Ok(await _service.GetPerformanceReportAsync());
+    }
+}
