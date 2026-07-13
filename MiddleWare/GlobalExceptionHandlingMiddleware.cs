25183edd5e15e3023985cf5a302b78a23752e883
saiteja-snad <saiteja.anumala@snaddevelopers.com>
2026-07-13 18:36:20 +0530

    Add files via upload

---
+++ MiddleWare/GlobalExceptionHandlingMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+
+namespace SMS.Middleware
+{
+    public class GlobalExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public GlobalExceptionHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                context.Response.ContentType = "application/json";
+
+                await context.Response.WriteAsJsonAsync(new
+                {
+                    StatusCode = context.Response.StatusCode,
+                    Message = ex.Message,
+                    InnerException = ex.InnerException?.Message
+                });
+            }
+        }
+    }
+    }
